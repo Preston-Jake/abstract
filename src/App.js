617f92eef58a4e123ffb69ba481f0c289a462469
JakePreston <prestonjak@outlook.com>
2020-08-11 18:07:57 -0500

    infinite scrolling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import UnsplashImage from "./UnsplashImage";
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
+
+    fetch(`${apiRoot}/photos/random?client_id=${accessKey}&count=10`)
+      .then((res) => res.json())
+      .then((data) => setImages([...images, ...data]));
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Unsplash Infinite</h1>
       </header>
+        {images.map(image => {
+          return <UnsplashImage key={image.id} url={image.urls.regular}/>
+        })}
     </div>
   );
 }
